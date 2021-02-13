b39dcf9a8c1f144bed911ccfa46e0bd0ab93de1f
micnekr <micnekr@users.noreply.github.com>
2021-02-13 21:25:41 +0000

    deploy: 607a14cf7d6ce296f5ca5b12a228f361e7863c3e

---
+++ _nuxt/static/1613251533/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fachievements","\u002FjsProjects","\u002Fprofile","\u002Fprojects","\u002FprojectPages\u002FAFINN","\u002FprojectPages\u002Fcollatz","\u002FprojectPages\u002Fmaze"]})
