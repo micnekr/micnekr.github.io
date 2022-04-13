8365abfa0e7cc78a4ae346fc0f0cddab05d3b5ed
micnekr <misha.nekrasov@gmail.com>
2022-04-13 15:21:26 +0100

    Updates

--- _nuxt/static/1649859269/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fprofile","\u002Fachievements","\u002FjsProjects","\u002Fprojects","\u002FprojectPages\u002Fcollatz","\u002FprojectPages\u002FAFINN","\u002FprojectPages\u002Fmaze"]})
