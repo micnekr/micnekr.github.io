d012d01258ba680e3d67c46350b0fa09b7b9e389
micnekr <misha.nekrasov@gmail.com>
2022-04-13 15:03:53 +0100

    Updates

---
+++ _nuxt/1ebdff9.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{291:function(n,e,t){"use strict";t.r(e);var c=t(42),component=Object(c.a)({},(function(){var n=this,e=n.$createElement;return(n._self._c||e)("section",[n._v("This is not finished yet")])}),[],!1,null,null,null);e.default=component.exports}}]);
---
+++ _nuxt/static/1649858555/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fprofile","\u002Fachievements","\u002Fprojects","\u002FjsProjects","\u002FprojectPages\u002FAFINN","\u002FprojectPages\u002Fcollatz","\u002FprojectPages\u002Fmaze"]})
